822f2e88325892e00acf7f36df293f6fc031aa1f
menot255 <40204213+menot255@users.noreply.github.com>
2018-06-26 18:23:24 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const robot = new Discord.Client();
 var p = "!"
 
 function status1() {
-    robot.user.setActivity('На говнокодера',{ type: "WATCHING" })
+    robot.user.setActivity('На говнокодера',{ type: "WATCHING" });
     robot.user.setStatus('online')
 }
 robot.login(process.env.SECRET);
