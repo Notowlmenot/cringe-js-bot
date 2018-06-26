ae4564485bdbd00dbfd5c6145aa211547c08e803
menot255 <40204213+menot255@users.noreply.github.com>
2018-06-26 18:47:14 +1100

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ function status1() {
     robot.user.setActivity('На говнокодера',{ type: "WATCHING" });
     robot.user.setStatus('online');
 }
+robot.on('message', message => {
+    if(message.content.startsWith(p + 'help')) {
+        const embed = new Discord.RichEmbed()
+            .setTitle("Помощь")
+            .setColor("#00BFFF")
+            .setDescription('Мои команды:')
+            .setFooter("Напиши что хочешь")
+            .setTimestamp();
+        message.channel.send({embed}).then(sentMessage => {   
+            sendMessage.react('🇭')
+                .then(() => sentMessage.react('🇪'))
+                    .then(() => sentMessage.react('🇱'))
+                    .then(() => sentMessage.react('🇵'))
+                    .catch(() => console.error('One of the emojis failed to react.'));
+        });
+    }
+});
 robot.login(process.env.SECRET);
