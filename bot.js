7aec3a32a5916a4916f78b72565878e2c78d69ef
menot255 <40204213+menot255@users.noreply.github.com>
2018-06-26 16:38:04 +1100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const request = require('request');
+const robot = new Discord.Client();
+var p = "!"
+
+function status1() {
+    robot.user.setActivity('На говнокодера',{ type: "WATCHING" })
+    robot.user.setStatus('online')
+}
+client.login("process.env.BOT_TOKEN");
