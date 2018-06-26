8228943522b9a6ff8a3251d9a202d18f566a03a9
menot255 <40204213+menot255@users.noreply.github.com>
2018-06-26 20:09:58 +1100

    Update index.js

--- index.js
+++ index.js
@@ -24,4 +24,23 @@ robot.on('message', message => {
         });
     }
 });
+robot.on('message', message => {
+	const args = message.content.slice(p.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+	if(message.content.startsWith(p + 'poll')) {
+		message.delete().catch(O_o => {});
+		const say_poll_embed = args.join(" ");
+		const embed = new Discord.RichEmbed()
+			.setColor(`#00FFFF`)
+			.setDescription(say_poll_embed)
+			.setFooter("голосование|HEE4")
+			.setTimestamp();	
+            message.channel.send({
+                embed
+            }).then(function(message) {
+                message.react("✅")
+                message.react("❎")
+            }).catch(function() {});
+        }
+    }); 
 robot.login(process.env.SECRET);
