0505713d41ae409182ed14070aa85175a84dcb61
menot255 <40204213+menot255@users.noreply.github.com>
2018-06-27 11:47:11 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const request = require('request');
+const robot = new Discord.Client();
+var p = "!";
+robot.on('ready', () => {
+    robot.user.setActivity('loading..',{ type: "PLAYING" })
+    robot.user.setStatus('dnd')
+    setTimeout(status1, 16000)
+    console.log('ready launched bot...')
+});
+
+robot.on('message', message => {
+    if(message.content.startsWith(p + 'help')) {
+        const embed = new Discord.RichEmbed()
+            .setTitle("Помощь")
+            .setColor("#00BFFF")
+            .setDescription('Мои команды:')
+            .setFooter("Напиши что хочешь")
+            .setTimestamp();
+        message.channel.send({embed}).then(sentMessage => {   
+            sendMessage.react('🇭')
+                .then(() => sentMessage.react('🇪'))
+                    .then(() => sentMessage.react('🇱'))
+                    .then(() => sentMessage.react('🇵'))
+                    .catch(() => console.error('One of the emojis failed to react.'));
+        });
+    }
+});
+robot.on('message', message => {
+	if(message.content.startsWith(p + 'web')) {
+		const hook = new Discord.WebhookClient('461098819585835009', 'tHAmrnW0DBLWdkpudsyV6t6ukn7mVXDXDj3Z98HuO9oSw6guD5PaHye3w1yrOvHgfXvx');
+		hook.send('hello bitches')
+	};
+});
+    robot.user.setActivity('На своего кодера',{ type: "WATCHING" });
+    robot.user.setStatus('IDLE');
+}
+robot.login(process.env.SECRET);
