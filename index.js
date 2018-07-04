021e109445deee0b289b473d05e1c63a4680cb11
menot255 <40204213+menot255@users.noreply.github.com>
2018-07-04 16:57:32 +1100

    Update index.js

--- index.js
+++ index.js
@@ -102,6 +102,6 @@ robot.on('message', message => {
 message.channel.send('Pinging...').then(sent => {
     sent.edit(`Pong! Took ${sent.createdTimestamp - message.createdTimestamp}ms`);
 	})
-})
+}
 
 robot.login(process.env.SECRET);
