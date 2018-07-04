ad96c7c8f1ae83b118db71068b27ec3172fe6d4f
menot255 <40204213+menot255@users.noreply.github.com>
2018-07-04 18:52:52 +1100

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ robot.on('message', message => {
 robot.on('message', message => {
 	if(message.content.startsWith(p + 'kick')) {
 		var ygy = "Успешно"
-	var kicklul = message.mentions.members
+	const kicklul = message.mentions.members
 	member.kick(kicklul)
 		message.react('380571016994226186')
 		message.channel.send('ygy')
