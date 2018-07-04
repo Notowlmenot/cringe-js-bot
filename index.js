62227827c4232f5c920cc1ee61417ae0d834b77e
menot255 <40204213+menot255@users.noreply.github.com>
2018-07-04 16:34:53 +1100

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ robot.on('message', message => {
 		message.channel.send('ygy')
 	}
 });
-client != message => {
+robot.on('message', message => {
 ('message', message => {
   if (message.content === 'ping') {
      const pingsmessage = robot.ping
