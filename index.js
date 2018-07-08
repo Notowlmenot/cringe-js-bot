90a8fa66b8008ddfb7ad8d623bc164120849db54
menot255 <40204213+menot255@users.noreply.github.com>
2018-07-08 13:20:37 +1100

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ robot.on('guildMemberAdd', (member) => {
 });
 robot.on('message', message => {
 	if(message.content.startsWith(p + 'delmsg')) {
-				   if(!message.member.roles.some(r=>["Джс - плоха", "Неадекват"].includes(r.name)) )
+				   if(!message.member.roles.some(r=>["Админы"].includes(r.name)) )
       return message.reply("Прости, но ты не можешь использовать это!")
 		message.delete()
 		let delmes = message.content.slice((p + 'delmsg').length);
