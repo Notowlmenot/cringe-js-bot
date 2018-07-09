565a88dc123091034012c415a825921fdc959a49
menot255 <40204213+menot255@users.noreply.github.com>
2018-07-09 12:51:29 +1100

    Update index.js

--- index.js
+++ index.js
@@ -129,11 +129,6 @@ robot.on('message', message => {
 			vot.react('380570863873032192') ///Тхонк
 			vot.react('➖');	 ///минус
 			message.delete()
-			robot.on('message', message => {
-				if(message.content === 'vote') {
-				message.reply('используйте  (.vote сообщение) ')
-				}
-})
         })
     }
 });
