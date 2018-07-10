b07f31d6cfe766e2575bdfcb94bc2bc4fda61f4e
menot255 <40204213+menot255@users.noreply.github.com>
2018-07-10 16:12:13 +1100

    Update test.js

--- commands/moderation/test.js
+++ commands/moderation/test.js
@@ -1,7 +1,1 @@
-const Discord = require('discord.js');
 
-client.on('message', message => {
-                if(message.content == message.content.toUpperCase())
-                    message.reply('не капсь')
-    });
-};
