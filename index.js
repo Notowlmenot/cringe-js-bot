2792eb63e8d024f41ef44254bb41d595c73b9e6a
menot255 <40204213+menot255@users.noreply.github.com>
2018-07-10 16:11:41 +1100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const Discord = require('discord.js');
 const request = require('request');
 const robot = new Discord.Client();
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands/moderation').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-}
 var p = ('.');
 robot.on('ready', () => {
     robot.user.setActivity('loading..',{ type: "PLAYING" })
@@ -171,4 +165,40 @@ robot.on('message', message => {
         })
     }
 });
+const Social = require("../../structures/Social.js");
+const { get } = require("snekfetch");
+
+class Dog extends Social {
+  constructor(...args) {
+    super(...args, {
+      name: "dog",
+      description: "Post a randomly selected image of a dog.",
+      category: "Animals",
+      usage: "dog",
+      extended: "This command will return a beautiful dog.",
+      cost: 5,
+      cooldown: 10,
+      aliases: ["doggo", "pupper"],
+      loadingString: "<a:typing:397490442469376001> **{{displayName}}** is petting a dog...",
+      botPerms: ["EMBED_LINKS"]
+    });
+  }
+
+  async run(message, args, level, loadingMessage) {
+    const { body } = await get(args[0] ? `https://dog.ceo/api/breed/${args[0]}/images/random` : "https://dog.ceo/api/breeds/image/random");
+    await loadingMessage.edit({
+      embed: {
+        "title": "Click here if the image failed to load.",
+        "url": body.message,
+        "color": 6192321,
+        "image": {
+          "url": body.message
+        }
+      }
+    });
+
+  }
+}
+
+module.exports = Dog;
 robot.login(process.env.SECRET);
