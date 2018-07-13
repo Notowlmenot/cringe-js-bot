4484eca014b6d6d9955c70ae0ef554540f003117
menot255 <40204213+menot255@users.noreply.github.com>
2018-07-13 23:46:25 +1100

    Update index.js

--- index.js
+++ index.js
@@ -172,5 +172,23 @@ robot.on('message', message => {
 		});
 		message.delete()
 	}
-})
+});
+client.on("message", message => {
+  const args = message.content.split(" ").slice(1);
+
+  if (message.content.startsWith(p + 'eval')) {
+    if(message.author.id !== config.ownerID) return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+});
 robot.login(process.env.SECRET);
