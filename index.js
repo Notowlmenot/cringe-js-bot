27db3982913cbee445540777414e801121c7bbae
menot255 <40204213+menot255@users.noreply.github.com>
2018-07-20 21:31:44 +1100

    Update index.js

--- index.js
+++ index.js
@@ -231,7 +231,7 @@ console.log('ник был сменен на' + vtes)
 });
 robot.on('message', message => {
 	if(message.content.startsWith(message)) {
-		.then(async (vot) => {
+		message.then(async (vot) => {
 				await vot.react('🇵') ///п
 				await vot.react('🇮') ///и
 				await vot.react('🇩');     ///
