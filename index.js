9aa066fe98fb0e7c75c1353ea7900ed0b22cd74a
menot255 <40204213+menot255@users.noreply.github.com>
2018-07-24 14:03:29 +1100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ robot.on('ready', () => {
 robot.on('message', message => {
     if(message.content.startsWith(p + 'help')) {
 	message.channel.send('А ты что ожидал увидеть?')
-	    console.log('${member.displayName} прописал команду help')
+	    console.log(`${member.displayName} прописал команду help`)
         };
 });
 function status1() {
@@ -30,7 +30,7 @@ robot.on('message', message => {
       return message.reply("Прости, но ты не можешь использовать это!")
         let say = message.content.slice((p + 'say').length);
         message.channel.send(say);
-	     console.log('${member.displayName} сказал' + say)
+	     console.log(`${member.displayName} сказал` + say)
     }
 });
 var messagelol = 'Я работаю!'
@@ -38,7 +38,7 @@ robot.on('message', message => {
     if(message.content === (p + 'check')) {
 	    message.channel.send(messagelol);
 	    message.react('380571016994226186')
-	     console.log('${member.displayName} чекнул бота на роботоспособность')
+	     console.log(`${member.displayName} чекнул бота на роботоспособность`)
     }
 });
 robot.on('message', message => {
@@ -48,7 +48,7 @@ robot.on('message', message => {
             .setColor('RANDOM')
 		.setImage(message.guild.iconURL)
 		 message.channel.send({embed})
-		console.log('показал логотип сервера ${member.guild.name} для ${member.displayName}')
+		console.log(`показал логотип сервера ${member.guild.name} для ${member.displayName}`)
 	}
 });
 robot.on('message', message => {
@@ -59,7 +59,7 @@ robot.on('message', message => {
             .setColor('RANDOM')
 		 .setImage(message.mentions.users.first().avatarURL)
 		 message.channel.send({embed})
-		console.log('показал аватар ${mentions1[0]} для ${member.displayName} в ${member.guild.name}')
+		console.log(`показал аватар ${mentions1[0]} для ${member.displayName} в ${member.guild.name}`)
 	}
 });
 robot.on('message', message => {
@@ -70,15 +70,7 @@ robot.on('message', message => {
 	message.mentions.members.first().kick()
 var mentions1 = message.mentions
 		message.channel.send('Успешно кикнут!')
-		console.log('${member.displayName} кикнул ${mentions1[0]} в ${member.guild.name}')
-	}
-});
-robot.on('message', message => {
-	if(message.content.startsWith(p + 'ha gey')) {
-		if(message.author.id !== '292178755760422915')
-			     return message.reply("Лул, команда только для Дора, смирись братан!")
-	message.mentions.members.first().addRole('371445035251662869')
-	message.delete()
+		console.log(`${member.displayName} кикнул ${mentions1[0]} в ${member.guild.name}`)
 	}
 });
 robot.on('message', message => {
@@ -86,7 +78,7 @@ robot.on('message', message => {
 message.channel.send('Pinging...').then(sent => {
     sent.edit(`Pong! Took ${sent.createdTimestamp - message.createdTimestamp}ms`);
 	var pingses = sent.createdTimestamp - message.createdTimestamp
-	console.log('${member.displayName} узнал пинг бота, он равен ' + pingses)
+	console.log(`${member.displayName} узнал пинг бота, он равен ` + pingses)
     });
   }
 });
@@ -95,7 +87,7 @@ robot.on('message', message => {
 message.channel.send('Pinging...').then(sent => {
     sent.edit(`Понг! пинг бота: ${sent.createdTimestamp - message.createdTimestamp}мс`);
 var pingses = sent.createdTimestamp - message.createdTimestamp
-	console.log('${member.displayName} узнал пинг бота! Он раверн ' + pingses)
+	console.log(`${member.displayName} узнал пинг бота! Он раверн ` + pingses)
     });
   }
 });
@@ -113,57 +105,11 @@ robot.on('message', message => {
 				await vot.react('➕') ///Плюс
 				await vot.react('380570863873032192') ///Тхонк
 				await vot.react('➖');     ///минус
+				console.log(`${member.displayName} начал голосование`);
 			})
 		});
 		message.delete()
 	}
-});
-robot.on('message', message => {
-	if(message.content.startsWith(p + 'mute')) {
-				message.delete()
-		if(!message.member.roles.some(r=>["Админы"].includes(r.name)) )
-		return message.reply("Прости, но ты не можешь использовать это!")
-		var keke = message.mentions.members.first()
-message.mentions.members.first().removeRole('425149859712991262')
-		message.mentions.members.first().addRole('424399811379200002')
-		let mutes = message.content.slice((p + 'mute' + keke).length);
-		var kekmute = mutes * 1000
-		var lulmute = kekmute * 60
-		var result = 'замучен до того момента, как его величеству заблагорассудится размутить'
-		setTimeout(()=>{message.mentions.members.first().addRole('425149859712991262')
-			       message.mentions.members.first().removeRole('424399811379200002')
-			       console.log('Пользователь должен быть размучен')},lulmute)
-									  message.reply(result).then((res) => {	
-			setTimeout(()=>{res.delete()},5000)
-        })
-    }
-});
-robot.on('message', message => {
-	if(message.content.startsWith(p + 'unmute')) {
-		message.delete()
-		var result = 'Пользователь размучен!'
-		if(!message.member.roles.some(r=>["Админы"].includes(r.name)) )
-			     return message.reply("Прости, но ты не можешь использовать это!")
-		message.mentions.members.first().addRole('425149859712991262')
-		message.mentions.members.first().removeRole('424399811379200002')
-				message.reply(result).then((res) => {
-        setTimeout(()=>{res.delete()},5000)
-        })
-    }
-});
-robot.on('message', message => {
-	if(message.content.startsWith(p + 'karta')) {
-		let votes = message.content.slice((p + 'test').length);
-		robot.fetchWebhook('466184206842855424', 'VXfIvJHBVLM2Ay7CGbVi60gguaECK-KjRC9GPee4vmVc0SCguidBuQB2AO7lknfx9YGZ').then(webhook => {
-			webhook.send(votes, {username: message.member.displayName, avatarURL: message.author.avatarURL}).then(async (vot) => {
-				await vot.react('➕') ///Плюс
-				await vot.react('380570863873032192') ///Тхонк
-				await vot.react('➖');     ///минус
-			})
-		});
-		message.delete() 
-	}
-console.log('${member.displayName} начал голосование за карту')
 });
 robot.on("message", message => {
   if (message.content.startsWith(p + 'eval')) {
@@ -205,7 +151,7 @@ if(message.author.id !== '292178755760422915')
 			     return message.reply("Прости, но ты не можешь использовать это!")
 message.delete()
 message.mentions.members.first().setNickname(vtes)
-console.log('${member.displayName} сменил ник ${mentions1[0]}' + vtes)
+console.log(`${member.displayName} сменил ник ${mentions1[0]}` + vtes)
 	}
 });
 robot.on("messageDelete", (msg) => {
